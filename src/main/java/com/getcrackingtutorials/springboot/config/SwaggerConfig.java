901983fc1329dbfcffb5dd92d04793a4bedb6e91
marcfernandes <marcfernandes@teksystems.com>
2022-06-23 11:58:24 -0400

    add different application properties files

--- src/main/java/com/getcrackingtutorials/springboot/config/SwaggerConfig.java
+++ src/main/java/com/getcrackingtutorials/springboot/config/SwaggerConfig.java
@@ -4,11 +4,15 @@ import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${text.message}")
+    private String test;
 
     @Bean
     public OpenAPI springbootOpenAPI() {
@@ -20,6 +24,11 @@ public class SwaggerConfig {
                 .externalDocs(new ExternalDocumentation()
                         .description("SpringBoot Wiki Documentation")
                         .url("https://springboot.wiki.github.org/docs"));
+    }
+
+    @Bean
+    public String whatever() {
+        return test;
     }
 
 }
